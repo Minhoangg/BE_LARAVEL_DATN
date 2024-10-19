f09c4cfc15bedaa8f8d0019993bc5079b6db3790
PC05708 <136137113+PC05708@users.noreply.github.com>
2024-10-19 13:26:08 +0700

    update product detail in client

--- routes/client/product/product.php
+++ routes/client/product/product.php
@@ -6,4 +6,5 @@ use App\Http\Controllers\client\product\ProductController;
 Route::prefix('/product')->group(function () {
     Route::get('/list', [ProductController::class, 'show']); //->middleware('auth.jwt')
     Route::get('show-all-chill/{id}', [ProductController::class, 'showAllChill']); //->middleware
+    Route::get('detail/{id}', [ProductController::class, 'detail']); //->middleware
 });
