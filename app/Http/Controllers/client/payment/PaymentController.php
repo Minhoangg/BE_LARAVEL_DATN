065ef91d50281ed48c88d46ad20467859a267a44
Minhoangg <132485081+Minhoangg@users.noreply.github.com>
2024-10-19 14:57:13 +0700

    update payment

--- app/Http/Controllers/client/payment/PaymentController.php
+++ app/Http/Controllers/client/payment/PaymentController.php
@@ -42,15 +42,33 @@ class PaymentController extends Controller
             'referenceCode' => $request->input('referenceCode'),
         ]);
 
+        $content = $request->input('content');
+        preg_match('/MDH\w+/', $content, $matches);
+
+        if (!empty($matches)) {
+            $sku_order = $matches[0];
+            $order = OrderModel::where('sku_order', $sku_order)->first();
+
+            if ($order) {
+                $order->paymend_status_id = 2;
+                $order->payment_at = now();
+                $order->save();
+
+                return response()->json(['message' => 'Payment status updated successfully.', 'transaction' => $transaction, 'order' => $order]);
+            } else {
+                return response()->json(['message' => 'Order not found.'], 404);
+            }
+        }
+
         return response()->json($transaction);
     }
 
-    public function getbyid(Request $request,$id){
+    public function getbyid(Request $request, $id)
+    {
 
 
         $transaction = SePayTransaction::find($id);
 
         return response()->json($transaction);
-
     }
 }
